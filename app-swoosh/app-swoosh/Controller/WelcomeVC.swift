751ebe11168bcd00f3ab904250b2bd6288f63563
Oliver Toohey <ottoohey@gmail.com>
2017-12-08 22:20:07 +1100

    Segues Programatically

---
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Oliver Toohey on 23/11/17.
+//  Copyright © 2017 Oliver Toohey. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
+
